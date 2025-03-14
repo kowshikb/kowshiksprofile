c9b71a23570679e0f2486ba9e2056736f0378954
kkbikkina <kowshik.bikkina@skillsoft.com>
2025-03-14 13:27:58 +0530

    .
    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/kowshiksprofile/", // Use "/" locally and "/kowshiksprofile/" in production
   server: {
     host: "::",
     port: 8080,
   },
-  base: mode === "production" ? "/kowshiksprofile/" : "/", // Use "/" locally and "/kowshiksprofile/" in production
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
